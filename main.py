13a602d24b7cad27df1b5eb5392acb8692718c43
arjun02003 <arjunchowhan94@gmail.com>
2026-02-02 09:29:14 -0800

    Implement No-Show Prediction API with FastAPI

---
+++ main.py
@@ -0,0 +1,31 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import joblib
+import pandas as pd
+
+app = FastAPI()
+
+model = joblib.load("xgboost_no_show_model.pkl")
+
+class Patient(BaseModel):
+    Age: int
+    LeadTimeDays: int
+    ScheduledHour: int
+    AppointmentDayOfWeek: int
+    SMS_received: int
+    PastNoShows: int
+    Scholarship: int
+    Hipertension: int
+    Diabetes: int
+    Alcoholism: int
+    Handcap: int
+
+@app.get("/")
+def home():
+    return {"status": "No-Show Prediction API is running"}
+
+@app.post("/predict")
+def predict(data: Patient):
+    df = pd.DataFrame([data.dict()])
+    prob = model.predict_proba(df)[0][1]
+    return {"no_show_risk": round(float(prob), 3)}
